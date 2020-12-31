3a1487aeabe02816be2c56616033195f4d1190e7
Amar Persaud <tehspartaa@gmail.com>
2020-12-31 05:01:49 -0500

    Tile and map

---
+++ EV2/Tile.cs
@@ -0,0 +1,14 @@
+﻿using MonoGame.CExtended;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EV2
+{
+    public struct Tile
+    {
+        public string Name { get; }
+        public int ID { get; }
+        public Sprite Sprite { get; }
+    }
+}
---
+++ EV2/TileMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EV2
+{
+    public class TileMap
+    {
+        public int[,] Map;
+        public List<Tile> Tiles;
+
+        public int Width { get; }
+        public int Height { get; }
+        public TileMap(int Width, int Height, List<Tile> Tiles)
+        {
+            this.Width = Width;
+            this.Height = Height;
+            Map = new int[Width, Height];
+            this.Tiles = Tiles; 
+        }
+    }
+}
